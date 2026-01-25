72c9c96563f70128d42cd822e5570a27f421e19c
ShohanHelal <shohanhelal725@gmail.com>
2026-01-25 22:18:32 +0600

    Starts Java Swing

---
+++ JAVA Swing/Dialog_Window/Message_Dialog_1.java
@@ -0,0 +1,9 @@
+package Dialog_Window;
+
+import javax.swing.JOptionPane;
+
+public class Message_Dialog_1 {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hellow");//'null' Decides windows Position
+	}
+}
