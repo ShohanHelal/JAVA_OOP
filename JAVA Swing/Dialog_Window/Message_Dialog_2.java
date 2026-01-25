bb24a43af02f6b3ba519dce12c797397f20df458
ShohanHelal <shohanhelal725@gmail.com>
2026-01-25 22:19:45 +0600

    some message dialoag lession

---
+++ JAVA Swing/Dialog_Window/Message_Dialog_2.java
@@ -0,0 +1,10 @@
+package Dialog_Window;
+
+import javax.swing.JOptionPane;
+
+public class Message_Dialog_2 {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hellow,how are you ?","Greeting",JOptionPane.QUESTION_MESSAGE);//("Position","Message","Title","Icon(-1,0,1,2,3)")
+		
+	}
+}
